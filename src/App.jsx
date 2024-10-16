672cfebd25826a19103802959acc90a32f2403ba
rvchougule <rohanchougule090@gmail.com>
2024-10-16 12:32:29 +0530

    Implementaing spotify clone using react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import Display from "./components/Display";
+import Player from "./components/player";
+import SideBar from "./components/SideBar";
+import { PlayerContext } from "./contexts/PlayerContext";
+
+export default function App() {
+  const { audioRef, track } = useContext(PlayerContext);
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <SideBar />
+        <Display />
+      </div>
+      <Player />
+      <audio ref={audioRef} src={track.file} preload="auto"></audio>
+    </div>
+  );
+}
